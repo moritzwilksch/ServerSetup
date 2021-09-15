9f725b5033f852954e4fbe21c770e92806a4ae88
Moritz Wilksch <58488209+moritzwilksch@users.noreply.github.com>
2021-09-15 17:58:52 -0400

    Update installSetup.sh

--- installSetup.sh
+++ installSetup.sh
@@ -2,3 +2,7 @@
 sudo apt-get install update
 sudo apt-get install htop git zsh wget make
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-x86_64.sh
+sudo chmod +x Miniforge3-Linux-x86_64.sh
+./Miniforge3-Linux-x86_64.sh
