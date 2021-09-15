0111b756e978a70c900cdf347f348f58d99d0067
Moritz Wilksch <58488209+moritzwilksch@users.noreply.github.com>
2021-09-15 18:14:35 -0400

    Update installSetup.sh

--- installSetup.sh
+++ installSetup.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
-sudo apt-get install update
-sudo apt-get install htop git zsh wget make
-sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
+# General systems update
+sudo apt-get install update
+
+# Install needed software
+sudo apt-get install htop git zsh wget make
+
+# Setup zshell and oh-my-zsh
+sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+sudo chsh -s /usr/bin/zsh $USER
+
+# Install miniconda
 wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-x86_64.sh
 sudo chmod +x Miniforge3-Linux-x86_64.sh
 ./Miniforge3-Linux-x86_64.sh
