606b8bd9315847e638f0381187b816ae70febe09
Moritz Wilksch <58488209+moritzwilksch@users.noreply.github.com>
2021-09-15 15:03:49 -0400

    Update installSetup.sh

--- installSetup.sh
+++ installSetup.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 sudo apt-get install update
-sudo apt-get install htop git zsh
+sudo apt-get install htop git zsh wget make
+sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
