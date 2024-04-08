69c1f649d5525ccb3faf1da58f963ae41bf20348
Moritz Wilksch <58488209+moritzwilksch@users.noreply.github.com>
2024-04-08 18:15:29 +0200

    simplify

--- moritz-setup.sh
+++ moritz-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
-packages=("git" "htop" "zsh" "wget" "curl" "make" "tmux" "fish" "ripgrep" "fzf" "gcc")
+cd ~
+packages=("git" "htop" "zsh" "wget" "curl" "make" "tmux" "fish" "ripgrep" "fzf" "gcc" "software-properties-common")
 
 # Function to check if a command is available
 command_exists() {
@@ -19,31 +19,15 @@ else
   SUDO=""
 fi
 
-# Check if yum is available
-if command_exists yum; then
-  echo "Using yum to install packages"
-  $SUDO yum install -y "${packages[@]}"
-# Check if apt is available
-elif command_exists apt; then
-  echo "Using apt to install packages"
-  $SUDO apt-add-repository ppa:fish-shell/release-3
-  $SUDO apt-get update
-  $SUDO apt-get install -y "${packages[@]}"
-else
-  echo "Error: Neither yum nor apt package manager found."
-  exit 1
-fi
 
-# Setup Zsh and Oh-My-Zsh
-# install nvim
-echo "Setting up Zsh and Oh-My-Zsh"
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
-sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-sudo chsh -s /usr/bin/zsh $USER
+$SUDO apt-add-repository -y ppa:fish-shell/release-3
+$SUDO apt-get update
+$SUDO apt-get install -y "${packages[@]}"
 
 # install nvim
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
-tar -xzf nvim-linux64.tar.gz && sudo mv nvim-linux64/bin/nvim /usr/bin/ && rm -rf nvim-linux64 nvim-linux64.tar.gz
+tar -xzf nvim-linux64.tar.gz && $SUDO mv nvim-linux64/ /usr/bin/ && rm -rf nvim-linux64.tar.gz
+mkdir -p ~/.config/
 cd ~/.config/ && git clone https://github.com/moritzwilksch/nvim.git && cd
 
 # Download dotfiles
@@ -52,11 +36,18 @@ mkdir -p ~/.config/fish
 wget -P ~/ https://raw.githubusercontent.com/moritzwilksch/dotfiles/main/.tmux.conf
 wget -P ~/.config/fish/ https://github.com/moritzwilksch/dotfiles/raw/main/.config/fish/config.fish
 
+# install pixi
+curl -fsSL https://pixi.sh/install.sh | bash
+echo 'pixi completion --shell fish | source' >> ~/.config/fish/config.fish
+
 # Define aliases
 echo "Defining aliases"
-echo "alias ll='ls -al'" >> ~/.zshrc
-echo "alias la='ls -al'" >> ~/.zshrc
-echo "alias mm='micromamba'" >> ~/.zshrc
-echo "alias ipy='ipython'" >> ~/.zshrc
-echo "alias ipyi='ipython -i'" >> ~/.zshrc
-source ~/.zshrc
+echo "alias ll='ls -al'" >> ~/.config/fish/config.fish
+echo "alias la='ls -al'" >> ~/.config/fish/config.fish
+echo "alias mm='micromamba'" >> ~/.config/fish/config.fish
+echo "alias ipy='ipython'" >> ~/.config/fish/config.fish
+echo "alias ipyi='ipython -i'" >> ~/.config/fish/config.fish
+echo "alias nvim=/usr/bin/nvim-linux64/bin/nvim" >> ~/.config/fish/config.fish
+
+# change login shell
+chsh -s /usr/bin/fish $USER
